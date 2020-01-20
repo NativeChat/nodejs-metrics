88f4b227146bbc6ed7e5c8f14e09db1cc46d7760
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2020-01-20 15:52:31 +0200

    Add tests

---
+++ spec/integration/metrics.spec.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const express = require('express');
+
+const { httpGet, verifyMetricsResponse } = require('../helpers/helpers');
+
+const { Metrics } = require('../../index');
+
+describe('Metrics', () => {
+  const metrics = new Metrics();
+
+  beforeAll(async () => {
+    await metrics.init();
+  });
+
+  afterAll(async () => {
+    await metrics.destroy();
+  });
+
+  describe('Server', () => {
+    it('should return default nodejs and http metrics.', async () => {
+      const expectedMetrics = [
+        '# TYPE process_cpu_user_seconds_total counter',
+        '# TYPE process_cpu_system_seconds_total counter',
+        '# TYPE process_cpu_seconds_total counter',
+        '# TYPE process_start_time_seconds gauge',
+        '# TYPE process_resident_memory_bytes gauge',
+        '# TYPE nodejs_eventloop_lag_seconds gauge',
+        '# TYPE nodejs_active_handles_total gauge',
+        '# TYPE nodejs_active_requests_total gauge',
+        '# TYPE nodejs_heap_size_total_bytes gauge',
+        '# TYPE nodejs_heap_size_used_bytes gauge',
+        '# TYPE nodejs_external_memory_bytes gauge',
+        '# TYPE nodejs_heap_space_size_total_bytes gauge',
+        '# TYPE nodejs_heap_space_size_used_bytes gauge',
+        '# TYPE nodejs_heap_space_size_available_bytes gauge',
+        '# TYPE nodejs_version_info gauge',
+        '# TYPE http_request_duration_seconds histogram'
+      ];
+
+      await verifyMetricsResponse(expectedMetrics);
+    });
+
+    it('should return express req/res metrics.', async () => {
+      const paramPath = '/v1/:someParam';
+      const staticPath = '/v1/static';
+      const port = 55555;
+      const appServerUrl = `http://localhost:${port}`;
+
+      await new Promise((resolve) => {
+        const app = express();
+
+        app.use(metrics.getMonitoringMiddleware());
+
+        app.get(staticPath, (req, res) => res.sendStatus(200));
+        app.get(paramPath, (req, res) => res.sendStatus(200));
+
+        app.listen(port, resolve);
+      });
+
+      await httpGet(appServerUrl + '/v1/test');
+      await httpGet(appServerUrl + '/v1/static');
+      await httpGet(appServerUrl + '/v1/not/found');
+
+
+      const expectedMetrics = [
+        `http_request_duration_seconds_bucket{le="+Inf",status_code="200",method="GET",path="${paramPath}"} 1`,
+        `http_request_duration_seconds_bucket{le="+Inf",status_code="200",method="GET",path="${staticPath}"} 1`,
+        'http_request_duration_seconds_count{status_code="404",method="GET",path="/__notFound__"} 1'
+      ];
+
+      await verifyMetricsResponse(expectedMetrics);
+    });
+  });
+});
---
+++ spec/unit/metrics.spec.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const { Metrics } = require('../../index');
+const sinon = require('sinon');
+
+const getBackend = (register = {}) => {
+  const backend = {
+    startServer: sinon.usingPromise().spy(),
+    stopServer: sinon.usingPromise().spy(),
+    getClient: sinon.stub().returns({ register })
+  };
+
+  return backend;
+};
+
+const getExpressMiddlewareProvider = () => {
+  const expressMiddlewareProvider = sinon.stub().returns(() => { });
+
+  return expressMiddlewareProvider;
+};
+
+const logger = { info: () => { } };
+
+const getMetrics = (register = {}) => {
+  const backend = getBackend(register);
+  const expressMiddlewareProvider = getExpressMiddlewareProvider();
+  const metrics = new Metrics({ logger, backend, expressMiddlewareProvider });
+
+  return { metrics, backend, expressMiddlewareProvider };
+};
+
+describe('Metrics', () => {
+  describe('init', () => {
+    it('should start the metrics server using the provided metrics backend.', async () => {
+      const { backend, metrics } = getMetrics();
+
+      await metrics.init();
+
+      expect(backend.startServer.calledOnce).toBeTrue();
+    });
+  });
+
+  describe('destroy', () => {
+    it('should stop the metrics server using the provided metrics backend.', async () => {
+      const { backend, metrics } = getMetrics();
+
+      await metrics.destroy();
+
+      expect(backend.stopServer.calledOnce).toBeTrue();
+    });
+  });
+
+  describe('getMonitoringMiddleware', () => {
+    it('should pass the register of the metrics backend to the express middleware provider.', async () => {
+      const expectedRegister = {};
+      const { expressMiddlewareProvider } = getMetrics(expectedRegister);
+
+      expect(expressMiddlewareProvider.calledOnceWithExactly(expectedRegister)).toBeTrue();
+    });
+  });
+});
