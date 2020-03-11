062b796b5cdba701e568e807780ec4789bfcc30d
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2020-03-11 16:13:24 +0200

    [CHAT-661]: Add MetricsTracker

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const { Metrics } = require('./lib/metrics');
 const { ExpressMiddlewareDefaultSettings } = require('./lib/constants');
+const { MetricsTracker } = require('./lib/metrics-tracker');
 
 module.exports = {
     Metrics,
-    ExpressMiddlewareDefaultSettings
+    ExpressMiddlewareDefaultSettings,
+    MetricsTracker
 };
